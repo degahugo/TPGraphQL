ad7ec57540ef2692c0826d9d7fa7798031e04e76
degahugo <79197414+degahugo@users.noreply.github.com>
2023-11-21 16:30:38 +0000

    Update 5 files

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,69 +1,95 @@
-import pets from "./database";
+import pets, { owners } from "./database";
 import { randomUUID } from "crypto";
 
-type Pet = {
-    id: string;
-    name: string;
-    age: number;
-    pictureUri: string;
-    ownerName: string;
+type Pet = (typeof pets)[0];
+type Owner = (typeof owners)[0];
+
+const getOwner = (args: { id: string }): Owner | undefined => {
+  return owners.find((owner) => owner.id === args.id);
+};
+const getOwners = () => {
+  return owners;
+};
+
+const createOwner = (args: Omit<(typeof owners)[0], "id">) => {
+  const generatedId = randomUUID().toString();
+  const owner = { id: generatedId, ...args };
+  owners.push(owner);
+  return owner;
+};
+const updateOwner = (args: (typeof owners)[0]) => {
+  const index = owners.findIndex((owner) => owner.id === args.id);
+  const owner = owners[index];
+
+  if (args.name) owner.name = args.name;
+  return owner;
+};
+
+const deleteOwner = (args: (typeof owners)[0]) => {
+  const index = owners.findIndex((owner) => owner.id === args.id);
+  if (index !== -1) {
+    owners.splice(index, 1);
+  }
+  return args.id;
 };
 
 const getPet = (args: { id: string }): Pet | undefined => {
-    return pets.find((pet: Pet) => pet.id === args.id);
+  return pets.find((pet: Pet) => pet.id === args.id);
 };
 
 const getPets = (): Pet[] => {
-    return pets;
+  return pets;
 };
 
-const createPet = (args: {
-    name: string;
-    age: number;
-    pictureUri: string;
-    ownerName: string;
-}): Pet => {
-    // generate randon uuid for pet object
-    const generatedId = randomUUID().toString();
-    // create pet object and save
-    const pet = { id: generatedId, ...args };
-    pets.push(pet);
-    return pet;
+const createPet = (args: Omit<Pet, "id">): Pet => {
+  // generate randon uuid for pet object
+  const generatedId = randomUUID().toString();
+  // create pet object and save
+  const pet = { id: generatedId, ...args };
+  const owner = getOwner({ id: args.ownerId });
+  pet.owner = owner;
+  pets.push(pet);
+  return pet;
 };
 
 const updatePet = (args: {
-    id: string;
-    name?: string;
-    age?: number;
-    pictureUri?: string;
-    ownerName?: string;
+  id: string;
+  name?: string;
+  age?: number;
+  pictureUri?: string;
+  ownerName?: string;
+  ownerid?: string;
 }): Pet => {
-    // loop through pets array and get object of pet
-    const index = pets.findIndex((pet: Pet) => pet.id === args.id);
-    const pet = pets[index];
+  // loop through pets array and get object of pet
+  const index = pets.findIndex((pet: Pet) => pet.id === args.id);
+  const pet = pets[index];
 
-    // update field if it is passed as an argument
-    if (args.age) pet.age = args.age;
-    if (args.name) pet.name = args.name;
-    if (args.pictureUri) pet.pictureUri = args.pictureUri;
+  // update field if it is passed as an argument
+  if (args.age) pet.age = args.age;
+  if (args.name) pet.name = args.name;
+  if (args.pictureUri) pet.pictureUri = args.pictureUri;
 
-    return pet;
+  return pet;
 };
 
 const deletePet = (args: { id: string }): string => {
-    // loop through pets array and delete pet with id
-    const index = pets.findIndex((pet: Pet) => pet.id === args.id);
-    if (index !== -1) {
-        pets.splice(index, 1);
-    }
+  // loop through pets array and delete pet with id
+  const index = pets.findIndex((pet: Pet) => pet.id === args.id);
+  if (index !== -1) {
+    pets.splice(index, 1);
+  }
 
-    return args.id;
+  return args.id;
 };
 
 export const root = {
-    getPet,
-    getPets,
-    createPet,
-    updatePet,
-    deletePet,
+  getPet,
+  getPets,
+  createPet,
+  updatePet,
+  deletePet,
+  createOwner,
+  deleteOwner,
+  updateOwner,
+  getOwners,
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,24 +1,33 @@
 import { buildSchema } from "graphql";
 
 const schema = buildSchema(`
+  type Owner {
+    id: ID!
+    name: String!
+  }
    type Pet {
     id: ID!
     name: String!
     age: Int!
     pictureUri: String
-    ownerName: String!
+    ownerId: ID!
+    owner:Owner
   }
-
   type Query {
+        getOwners: [Owner]
+        getOwner(id:ID!): Owner
         getPets: [Pet]
-        getPet(id: ID!): Pet
+        getPet(id:ID!): Pet
+        
     }
-
-  type Mutation {
-        createPet(name: String!, age: Int!, pictureUri: String, ownerName: String!): Pet!
-        updatePet(id: ID!, name: String, age: Int, pictureUri: String, ownerName: String): Pet!
-        deletePet(id: ID!): ID!
-    }
+    type Mutation {
+      createPet(name: String!, age: Int!, pictureUri: String, ownerId:
+      String!): Pet!
+      updatePet(id: ID!, name: String, age: Int, pictureUri: String,
+      ownerId: String): Pet!
+      deletePet(id: ID!): ID!
+      createOwner(name: String!): Owner!
+      }
 `);
 
 export default schema;
